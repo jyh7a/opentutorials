a5543928d547100b33d4b5d0a56626e052801844
jyh7a <jyh7a@naver.com>
2020-06-29 01:06:56 +0900

    main update

--- Node.js/web2-node.js/main.js
+++ Node.js/web2-node.js/main.js
@@ -1,16 +1,20 @@
 // 모듈 추출
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
 // 서버생성
 const app = http.createServer((req, res) => {
-  let url = req.url;
+  let _url = req.url;
+  let queryData = url.parse(_url, true).query;
+  console.log(queryData)
 
-  if(req.url == '/'){ url = '/index.html' }
-  else if(req.url == '/favicon.ico'){ return res.writeHead(404) }
+  if(_url == '/'){ _url = '/index.html' }
+  else if(_url == '/favicon.ico'){ return res.writeHead(404) }
+  
 
   res.writeHead(200);
-  console.log(__dirname + url)
-  res.end(fs.readFileSync(__dirname + url))
+  console.log(__dirname + _url)
+  // res.end(fs.readFileSync(__dirname + _url))
   
 }).listen(3000);
