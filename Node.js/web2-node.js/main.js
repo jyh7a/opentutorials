28aaa404e3769a1fbe09854268f4abcae8b50543
jyh7a <jyh7a@naver.com>
2020-06-30 00:22:06 +0900

    not found complete

--- Node.js/web2-node.js/main.js
+++ Node.js/web2-node.js/main.js
@@ -5,46 +5,45 @@ const url = require('url')
 
 // 서버생성
 const app = http.createServer((req, res) => {
-  let _url = req.url;
-  console.log(_url);
-  let queryData = url.parse(_url, true).query;
+  let _url = req.url
+  let queryData = url.parse(_url, true).query
+  let pathname = url.parse(_url, true).pathname
   let title = queryData.id
+
+  console.log(url.parse(_url, true))
+
+  if(pathname === '/'){
+    fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+      if(err) console.log(err)
   
-  if(_url == '/') title = 'Welcome' 
-  else if(_url == '/favicon.ico') return res.writeHead(404) 
+      let template = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8" />
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="1.html?id=HTML">HTML</a></li>
+          <li><a href="2.html?id=CSS">CSS</a></li>
+          <li><a href="3.html?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+    `
+    res.writeHead(200)
+    res.end(template)
+    })
+  }else{
+    res.writeHead(404)
+    res.end('Not Found')
+  }
+  
 
-  res.writeHead(200);
-  fs.readFile(`data/${title}`, 'utf8', (err, data) => {
-    if(err) console.log(err)
-
-    let template = `
-    <!DOCTYPE html>
-    <html>
-
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8" />
-    </head>
-
-    <body>
-      <h1><a href="/">WEB</a></h1>
-
-      <ul>
-        <li><a href="1.html?id=HTML">HTML</a></li>
-        <li><a href="2.html?id=CSS">CSS</a></li>
-        <li><a href="3.html?id=JavaScript">JavaScript</a></li>
-      </ul>
-
-      <h2>${title}</h2>
-
-      <p>
-        ${data}
-      </p>
-    </body>
-    </html>
-  `
-  res.end(template)
-  })
   
   
-}).listen(3000);
+}).listen(3000)
