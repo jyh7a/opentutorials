7329d18d8b8b9c2e8665fcace0dc3f75628f2284
jyh7a <jyh7a@naver.com>
2020-07-12 15:19:09 +0900

    files delete

--- Node.js/Nodejs&MySQL/nodejs/muse.js
+++
@@ -1,3 +0,0 @@
-const part = require('./mpart.js')
-console.log('part : ', part)
-part.f();
--- Node.js/Nodejs&MySQL/nodejs/readdir.js
+++
@@ -1,9 +0,0 @@
-const testFolder = './data'
-const fs = require('fs')
-
-fs.readdir(testFolder, (err, files) => {
-  console.log(files)
-  files.forEach(file => {
-    console.log(file)
-  })
-})
