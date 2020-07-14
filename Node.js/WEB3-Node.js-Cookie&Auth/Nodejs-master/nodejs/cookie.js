5d8b63745023e01ebf1c517e8cdf208e12788d7d
jyh7a <jyh7a@naver.com>
2020-07-15 08:38:31 +0900

    lodash disabled - security issue

--- Node.js/WEB3-Node.js-Cookie&Auth/Nodejs-master/nodejs/cookie.js
+++ Node.js/WEB3-Node.js-Cookie&Auth/Nodejs-master/nodejs/cookie.js
@@ -1,8 +1,9 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-  // res.writeHead(200,{
-  //   'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-  // })
+  console.log(req.headers.cookie);
+  res.writeHead(200,{
+    'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+  })
   res.end('Cookie!!')
-}).listen(3000)
+}).listen(3000) 
