5c34026ed79f213790f8d6517a85bfda2fe17a6f
jyh7a <jyh7a@naver.com>
2020-07-15 15:52:54 +0900

    Permanent cookies
    내일 다시실행하면
    Permanet cookie가 8.14 -> 8.15로 늘어나는지 확인

--- Node.js/WEB3-Node.js-Cookie&Auth/Nodejs-master/nodejs/cookie.js
+++ Node.js/WEB3-Node.js-Cookie&Auth/Nodejs-master/nodejs/cookie.js
@@ -1,9 +1,21 @@
 const http = require('http')
+const cookie = require('cookie')
 
 http.createServer((req, res) => {
-  console.log(req.headers.cookie);
+  let cookies = {}
+  if(req.headers.cookie !== undefined){
+    cookies = cookie.parse(req.headers.cookie);
+  }
+  console.log(cookies);
+  console.log(cookies.yummy_cookie);
+  console.log(cookies.tasty_cookie);
+
   res.writeHead(200,{
-    'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    'Set-Cookie': [
+      'yummy_cookie = choco',
+      'tasty_cookie = strawberry',
+      `Permanet = cookies; Max-Age = ${60*60*24*30}`
+    ]
   })
   res.end('Cookie!!')
 }).listen(3000) 
